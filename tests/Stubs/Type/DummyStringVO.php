bdbf8866e2f6a29c0a6c044816243540eed58dc4
Riddick <danijel.galic@live.com>
2026-06-24 13:29:17 +0200

    Add `ValueObject` unit tests, stubs, and PHPUnit workflow
    Implemented `ValueObjectTest` to verify the behavior of the `ValueObject` base class, covering the `equals` and `sameAs` methods. Introduced `DummyStringVO` and `DummyIntVO` stubs to facilitate testing. Added a reusable PHPUnit GitHub Actions workflow for automated test execution on pull requests. These changes enhance code reliability and ensure foundational test coverage for value objects.

---
+++ tests/Stubs/Type/DummyStringVO.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Project ecosystem
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2026-present The FireHub Project - All rights reserved
+ * @license https://opensource.org/license/Apache-2-0 Apache License, Version 2.0
+ *
+ * @php-version >=8.2
+ * @package Core\Tests
+ */
+
+namespace FireHub\Tests\Core\Stubs\Type;
+
+use FireHub\Core\Type\ValueObject;
+use InvalidArgumentException;
+
+/**
+ * ### Dummy string Value Object
+ * @since 1.0.0
+ */
+readonly class DummyStringVO extends ValueObject {
+
+    /**
+     * ### Constructor
+     * @since 1.0.0
+     *
+     * @param string $value
+     *
+     * @return void
+     */
+    public function __construct (
+        private string $value = 'default'
+    ) {
+
+        $this->guard(
+            fn() => $this->value !== '',
+            fn() => new InvalidArgumentException('Value cannot be empty.')
+        );
+
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since 1.0.0
+     */
+    public function value ():string {
+
+        return $this->value;
+
+    }
+
+}
